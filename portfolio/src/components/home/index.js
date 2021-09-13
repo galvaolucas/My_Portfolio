29259b1027dff733983467137727c5520341f03c
Lucas Galvao <lucasmelo_galvao@hotmail.com>
2021-09-12 23:57:08 -0300

    first commit

---
+++ portfolio/src/components/home/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './home.css'
+import Header from '../header'
+import Body from '../body'
+import Footer from '../footer'
+
+function home() {
+    return <div className = 'home'>
+        <div>
+            < Header /> 
+
+        </div>
+
+        <div>
+            < Body /> 
+
+        </div>
+
+        <div>
+            < Footer /> 
+
+        </div>
+
+    </div>
+        
+}
+
+export default home
